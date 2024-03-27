7b7a612c96306840cbb56a2e96f8937436a8c3f4
Pedro <pedro.lavado.contactor@bbva.com>
2024-03-27 13:14:19 +0100

    maquetacion

--- src/app/card-translate/card-translate.component.ts
+++ src/app/card-translate/card-translate.component.ts
@@ -1,14 +1,14 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-card-translate',
+  selector: "app-card-translate",
   standalone: true,
   imports: [
     CommonModule,
@@ -19,10 +19,30 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatIconModule,
   ],
-  templateUrl: './card-translate.component.html',
-  styleUrl: './card-translate.component.scss',
+  templateUrl: "./card-translate.component.html",
+  styleUrl: "./card-translate.component.scss",
 })
 export class CardTranslateComponent {
   @Input() translated: boolean = false;
-  value = '';
+  @Input() option: string = "";
+  @Input() value: string = "";
+  @Input() disabled: boolean = false;
+
+  @Output()
+  changeOption = new EventEmitter<string>();
+
+  @Output()
+  translate = new EventEmitter<string>();
+
+  changeOptionValue(option: string) {
+    this.changeOption.emit(option);
+  }
+
+  translateText() {
+    this.translate.emit(this.value);
+  }
+
+  copyText() {
+    navigator.clipboard.writeText(this.value);
+  }
 }
--- src/app/translate/translate.component.ts
+++ src/app/translate/translate.component.ts
@@ -1,15 +1,15 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatButtonModule } from '@angular/material/button';
-import { CardTranslateComponent } from '../card-translate/card-translate.component';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatButtonModule } from "@angular/material/button";
+import { CardTranslateComponent } from "../card-translate/card-translate.component";
 
 @Component({
-  selector: 'app-translate',
+  selector: "app-translate",
   standalone: true,
   imports: [
     CommonModule,
@@ -21,9 +21,24 @@ import { CardTranslateComponent } from '../card-translate/card-translate.compone
     MatButtonModule,
     CardTranslateComponent,
   ],
-  templateUrl: './translate.component.html',
-  styleUrl: './translate.component.scss',
+  templateUrl: "./translate.component.html",
+  styleUrl: "./translate.component.scss",
 })
 export class TranslateComponent {
-  value = '';
+  optionCard1 = "en";
+  optionCard2 = "es";
+  textCard1 = "";
+  textCard2 = "";
+
+  changeOption1(option: string) {
+    this.optionCard1 = option;
+  }
+
+  changeOption2(option: string) {
+    this.optionCard2 = option;
+  }
+
+  translateText(text: string) {
+    this.textCard2 = text + this.optionCard1 + this.optionCard2;
+  }
 }
